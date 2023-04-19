9bb1a5d2910b209d3c7f66c10bbbbd79cfe7f641
S-Caelum <deus.caelum@outlook.com>
2023-04-19 14:07:42 +0500

    Add project files.

---
+++ Kal2/Windows/AuthWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Kal2.Data;
+using Kal2.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kal2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string PasswordValue = PasswordTextBox.Password;
+            try
+            {
+                var User = DiagnosisEntities.getInstance().Employees.Where(p => p.AuthData1.AuthLogin == LoginTextBox.Text).First();
+                if (User == null)
+                {
+                    MessageBox.Show("Введён неверный логин");
+                }
+                else
+                {
+                    if (User.AuthData1.AuthPassword == PasswordValue)
+                    {
+                        switch (User.EmployeeRole)
+                        {
+                            case 1: onSuccessfullSignIn(User);
+                                    break;
+                            case 6: onSuccessfullSignIn(User);
+                                    break;
+                        }
+                    }
+                    else
+                        MessageBox.Show("Введён неверный пароль");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void onSuccessfullSignIn(Employee User)
+        {
+            MainWindow mainWindow = new MainWindow(User);
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Kal2/Windows/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Kal2.Data;
+using Kal2.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kal2.Windows
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(Employee User)
+        {
+            InitializeComponent();
+            string UserFullName = User.LastName.ToString() + " " + User.FirstName.ToString();
+            this.UserName.Content = $"Добро пожаловать, {UserFullName}";
+            switch (User.EmployeeRole)
+            {
+                case 1: MainFrame.Navigate(new Uri("Pages/AdminPage.xaml", UriKind.Relative));
+                        break;
+                case 6: MainFrame.Navigate(new Uri("Pages/UserPage.xaml", UriKind.Relative));
+                        break;
+            }
+        }
+    }
+}
